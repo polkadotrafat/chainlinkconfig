5abeb774ae43168a1e40e201b281373e2a8eabc4
Rafat Hussain <rafat.hsn@gmail.com>
2022-11-10 03:40:50 +0530

    dreapi.sol changed for the new api

--- drexapi.sol
+++ drexapi.sol
@@ -17,7 +17,7 @@ contract Client is ChainlinkClient {
     constructor() {
         setChainlinkToken(address(0xa36085F69e2889c224210F603D836748e7dC0088));
         oracle = 0x5e609E1c258255ef9Ee2022a519aBf5b9Fe1afe2;
-        jobId = "646207fab46f4499a88ae7c81cd8b073";
+        jobId = "cdc698f82bb44716ad4e9a57f10c6357";
         fee = 0;
     }
 
@@ -29,19 +29,15 @@ contract Client is ChainlinkClient {
         Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
 
         // Set the URL to perform the GET request on
-        request.add("get", "https://safe-retreat-69216.herokuapp.com/robonomics");
+        request.add("get", "http://drex-env.eba-jkxuyqbq.us-east-1.elasticbeanstalk.com/grid/last");
 
         // Set the path to find the desired data in the API response, where the response format is:
-        // {"RAW":
-        //   {"ETH":
-        //    {"USD":
-        //     {
-        //      "VOLUME24HOUR": xxx.xxx,
-        //     }
-        //    }
-        //   }
-        //  }
-        request.add("path", "data.energyacum");
+        
+        request.add("path", "energy-acum");
+
+        // Multiply the result by 1000000000000000000 to remove decimals
+        int timesAmount = 10**18;
+        request.addInt("times", timesAmount);
 
         // Sends the request
         return sendChainlinkRequestTo(oracle, request, fee);
